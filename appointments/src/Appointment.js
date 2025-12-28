3acd685f81c1272e7f4986f590286f3ab21c30cb
David Gómez G. <dgomezg@gmail.com>
2025-12-28 14:12:05 +0100

    Added AppointmentsDayView feature

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Appointment = ({ customer }) => (
+const appointmentTimeOfDay = startsAt => {
+    const [h, m] = new Date(startsAt).toTimeString().split(':');
+    return `${h}:${m}`;
+}
+export const Appointment = ({ customer }) => (
     <div>{customer.firstname}</div>
 );
 
-export default Appointment;
+export const AppointmentsDayView = ({ appointments }) => (
+    <div id='appointmentsDayView'>
+        <ol>
+            {appointments.map((appointment) => (
+                <li key={appointment.startsAt}>
+                    {appointmentTimeOfDay(appointment.startsAt)}
+                </li>
+            ))}
+        </ol>
+    </div>
+);
+
--- appointments/test/Appointments.test.js
+++ appointments/test/Appointments.test.js
@@ -1,6 +1,6 @@
 import React, { act } from 'react';
 import { createRoot } from 'react-dom/client';
-import Appointment from '../src/Appointment';
+import {Appointment, AppointmentsDayView} from '../src/Appointment';
 
 const flushMicrotasks = () => Promise.resolve();
 
@@ -35,4 +35,53 @@ describe('Appointment', () => {
         await render(<Appointment customer={customer}/>)
         expect(document.body.textContent).toMatch('Jordan');
     });
+});
+
+describe('AppointmentsDayView', () => {
+
+    let root;
+    let container;
+    const today = new Date();
+    const appointments = [
+        { startsAt: today.setHours(12,0)},
+        { startsAt: today.setHours(13,0)}
+    ];
+
+    const render = async component => {
+        await act(async () => {
+            root.render(component);
+            await flushMicrotasks();
+        });
+    };
+
+    beforeEach(() => {
+        container = document.createElement('div');
+        document.body.appendChild(container);
+        root = createRoot(container);
+    });
+
+    it('renders a div with the right id', async () => {
+        await render(<AppointmentsDayView appointments={[]}/>);
+        expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
+    })
+
+    it('renders multiple appointments in an ol element', async () => {
+        await render(<AppointmentsDayView appointments={appointments}/>);
+        expect(
+            container.querySelector('ol').children
+        ).toHaveLength(2);
+    })
+
+    it('renders each appointment in a li element', async () => {
+        await render(<AppointmentsDayView appointments={appointments}/>);
+        expect(
+            container.querySelectorAll('li')
+        ).toHaveLength(2);
+        expect(
+            container.querySelectorAll('li')[0].textContent
+        ).toEqual('12:00');
+        expect(
+            container.querySelectorAll('li')[1].textContent
+        ).toEqual('13:00');
+    })
 });
